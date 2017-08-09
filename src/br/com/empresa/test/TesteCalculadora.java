da746df41cdadc86c3fc3b6d7b382e35cc777b54
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-08-08 21:39:35 -0300

    refatoração da soma

--- src/br/com/empresa/test/TesteCalculadora.java
+++ src/br/com/empresa/test/TesteCalculadora.java
@@ -2,23 +2,38 @@ package br.com.empresa.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.empresa.lib.Calculadora;
 
 public class TesteCalculadora {
+	private Calculadora calculadora;
 
+	@Before
+	public void before() {
+		calculadora = new Calculadora();
+	}
+	
 	@Test
 	public void somaNumerosInteiros() {
-		Calculadora calculadora = new Calculadora();
-		assertEquals(calculadora.Soma(2,3), 5, 0); 
+		calculadora.Somar(3);
+		assertEquals(calculadora.getResultado(), 3, 0);
+		calculadora.Somar(7);
+		assertEquals(calculadora.getResultado(), 10, 0);
 	}
 
 	@Test
 	public void somaNumerosComVirgula() {
-		Calculadora calculadora = new Calculadora();
-		assertEquals(calculadora.Soma(2.33,6.59), 8.92, 0); 
+		calculadora.Somar(2.33);
+		assertEquals(calculadora.getResultado(), 2.33, 0); 
 	}
 	
-
+	@Test
+	public void criaCalculadoraComValorInicial() {
+		calculadora = new Calculadora(10);
+		assertEquals(calculadora.getResultado(), 10, 0);
+		calculadora.Somar(3);
+		assertEquals(calculadora.getResultado(), 13, 0); 
+	}	
 }
